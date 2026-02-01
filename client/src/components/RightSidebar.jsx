e96a873668711a92ecf823292b61452d3b0f1759
aniketashokbhise <aniketbhise449@gmail.com>
2026-02-01 10:34:52 +0530

    first commit

---
+++ client/src/components/RightSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RightSliderbar() {
+  return (
+    <div>
+      <h1>right</h1>
+    </div>
+  )
+}
+
+export default RightSliderbar
---
+++ client/src/lib/utils.js
@@ -0,0 +1,10 @@
+export function formatMessageTime(date = new Date()) {
+  return new Date(date).toLocaleTimeString("en-IN", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+    timeZone: "Asia/Kolkata"
+  });
+}
+ 
